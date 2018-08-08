12f8e60e6a0e987633a688595780e96db2ad31c0
JuunJunn <zhoujunjunlove@gmail.com>
2018-08-08 16:43:42 +0800

    finish index

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ const utilApi = require('../model/api')//数据处理Api
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   const result = await utilApi.findBloglist({limit: 10, offset: 0})
-  // res.send(JSON.stringify(result.data.list))
-  res.render('index', { blogList: result.data.list});
+  const blogList = result.data.list
+  res.render('index', { blogList: blogList});
 });
 
 module.exports = router;
