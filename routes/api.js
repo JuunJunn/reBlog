4ee238ad7572d10fa7afb68a7044e671252416a6
JuunJunn <zhoujunjunlove@gmail.com>
2018-08-10 17:19:19 +0800

    keep code

--- routes/api.js
+++ routes/api.js
@@ -17,4 +17,18 @@ router.get('/blog', async (req, res) => {
   }
 })
 
+
+//根据id查询单个blog对象 编辑页面
+router.get('/blog', async (req, res) => {
+  const id = parseInt(req.query.id)
+  if(id) {
+    const result = await utilApi.findById(id)
+    console.log(result.data)
+    res.render('detail', {blogData: result.data})
+  }else {
+    res.send('404 Not Found')
+  }
+})
+
+
 module.exports = router;
